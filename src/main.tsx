fe169b52167b61dbe3207930821c8a951b7ca4d8
Shivanshu garg <gargshivanshu000@gmail.com>
2026-07-02 18:09:13 +0530

    All Error fixed Gemini ai Changed to Groq ai

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,5 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./index.css";
 
-const root = document.getElementById("root");
-if (!root) throw new Error("Root element not found");
-
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
