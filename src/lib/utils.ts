615d41d934004a437ed2f49fa2346205eb6c6ed9
Shivanshu garg <gargshivanshu000@gmail.com>
2026-07-02 15:27:24 +0530

    All Error fixed Gemini ai Changed to Groq ai

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,7 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const API_BASE_URL = "http://localhost:5000";
+// export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://resumexbackend-2.onrender.com";
 
 export const apiUrl = (path: string) =>
   `${API_BASE_URL}${path.startsWith("/") ? path : `/${path}`}`;
